4c6e80d0ef60aabb2f1125302f4eb30359645346
IlluzionzDev <jaminstratford@gmail.com>
2020-05-28 17:17:26 +1200

    Add plugin controller

---
+++ src/main/java/com/illuzionzstudios/mist/controller/PluginController.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright © 2020 Property of Illuzionz Studios, LLC
+ * All rights reserved. No part of this publication may be reproduced, distributed, or
+ * transmitted in any form or by any means, including photocopying, recording, or other
+ * electronic or mechanical methods, without the prior written permission of the publisher,
+ * except in the case of brief quotations embodied in critical reviews and certain other
+ * noncommercial uses permitted by copyright law. Any licensing of this software overrides
+ * this statement.
+ */
+package com.illuzionzstudios.mist.controller;
+
+import com.illuzionzstudios.mist.plugin.SpigotPlugin;
+
+/**
+ * A tag that indicates a controller. A controller is class that handles things
+ * of a certain type. Has a start and stop method
+ *
+ * @param <P> The instance of the plugin this controller is for
+ */
+public interface PluginController<P extends SpigotPlugin> {
+
+    /**
+     * Starts up our controller
+     *
+     * @param plugin The plugin starting the controller
+     */
+    void initialize(P plugin);
+
+    /**
+     * Stops our controller
+     *
+     * @param plugin The plugin stopping the controller
+     */
+    void stop(P plugin);
+
+}
