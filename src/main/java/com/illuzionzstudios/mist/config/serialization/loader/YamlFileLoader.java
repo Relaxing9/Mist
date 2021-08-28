814b7dc5b2b6566f9f1c4fe7bce18bfda1d4a9a7
IlluzionzDev <jaminstratford@gmail.com>
2021-08-29 11:27:22 +1200

    Update yaml saving

--- src/main/java/com/illuzionzstudios/mist/config/serialization/loader/YamlFileLoader.java
+++ src/main/java/com/illuzionzstudios/mist/config/serialization/loader/YamlFileLoader.java
@@ -1,8 +1,12 @@
 package com.illuzionzstudios.mist.config.serialization.loader;
 
+import com.google.gson.*;
+import com.illuzionzstudios.mist.Logger;
 import com.illuzionzstudios.mist.config.YamlConfig;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Provides a way to load on object from a YAML file.
@@ -22,6 +26,25 @@ public abstract class YamlFileLoader<T> extends FileLoader<T> {
      */
     public YamlFileLoader(String directory, String fileName) {
         super(directory, fileName, "yml");
+    }
+
+    /**
+     * Save the object T into a json object to be saved to disk
+     */
+    public abstract void saveYaml();
+
+    /**
+     * Used to save our {@link JsonObject} to disk
+     * at {@link #file}
+     *
+     * @return If was saved successfully
+     */
+    @Override
+    public boolean save() {
+        // Load new object before saving
+        saveYaml();
+
+        return config.save();
     }
 
     @Override
