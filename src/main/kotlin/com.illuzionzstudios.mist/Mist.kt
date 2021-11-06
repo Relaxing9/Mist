88c9422c25bc729b512819b74a77971694d2ebf8
IlluzionzDev <jaminstratford@gmail.com>
2021-11-06 18:51:37 +1300

    Setup

---
+++ src/main/kotlin/com.illuzionzstudios.mist/Mist.kt
@@ -0,0 +1,7 @@
+package com.illuzionzstudios.mist
+
+class Mist {
+    fun test() {
+        println("Test")
+    }
+}
---
+++ src/main/kotlin/com.illuzionzstudios.mist/plugin/SpigotPlugin.kt
@@ -0,0 +1,9 @@
+package com.illuzionzstudios.mist.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SpigotPlugin: JavaPlugin() {
+
+
+
+}
