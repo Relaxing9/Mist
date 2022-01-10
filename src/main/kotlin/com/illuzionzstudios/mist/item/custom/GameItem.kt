6a89d20450f4107d440ea2b3f2e9e86902bdb39c
IlluzionzDev <jaminstratford@gmail.com>
2022-01-10 19:53:02 +1300

    Final version of 1.0 beta

--- src/main/kotlin/com/illuzionzstudios/mist/item/custom/GameItem.kt
+++
@@ -1,7 +0,0 @@
-package com.illuzionzstudios.mist.item.custom
-
-/**
- * Represents a GameItem object that can be interacted with. We
- * provide a Bukkit implementation of the object that is in the inventory, and we can add events for placing, using etc.
- */
-interface GameItem 
